c1d930f2c7d3d48acfc399da935163deb92788f9
tameem6 <tameem6ansar@gmail.com>
2019-04-24 15:22:44 +0530

    Basic routing

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var http= require('http');
 var fs = require('fs');
 
-var RS = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-
 
 
 http.createServer(function(req,res) {
-    res.writeHead(200,{'Content-type' : 'text/plain'});
-    RS.pipe(res);
+    if(req.url==='/' || req.url=='/home')
+    {
+        res.writeHead(200,{'Content-type':'text/html'});
+        var RS = fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res);
 
+    }
+    else if(req.url === '/api')
+    {
+        res.writeHead(200,{'Content-type' : 'application/json'});
+        var myObj= {
+            name: 'Random',
+            age:23,
+            gender: 'Male'
+        };
+        res.end(JSON.stringify(myObj));
+    }
+    else
+    {
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.end("404 Page Not Found");
+    }
 }).listen(8080);
 console.log("Server running");
